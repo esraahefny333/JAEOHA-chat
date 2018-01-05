cc286fb3f8bc64b092bef6e7fb5995006462dff2
esraahefny <esraahefny3@gmail.com>
2018-01-05 12:57:03 +0200

    Mlook l Java :D

---
+++ JAEOHA_1/src/rmi/ServerImpl/ServerImpl.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rmi.ServerImpl;
+
+import java.rmi.server.UnicastRemoteObject;
+import rmi.interfaces.ServerInterface;
+
+/**
+ *
+ * @author esraa
+ */
+public class ServerImpl extends UnicastRemoteObject implements ServerInterface{
+    
+}
